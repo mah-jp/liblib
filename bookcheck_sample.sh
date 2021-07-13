339aa9fce03b5adce7f5c7046ce07ccdb65b291c
mah-jp <masahiko.ohkubo+github@gmail.com>
2021-07-14 01:55:40 +0900

    内容整理

--- bookcheck_sample.sh
+++ bookcheck_sample.sh
@@ -3,15 +3,13 @@
 # Usage: $0
 
 PATH_ME=$(dirname $(readlink -f $0)) # Linuxなど (maxOSではだめね)
-#PATH_ME=$(dirname $0) # macOS向けの妥協 (symlink経由には対応できない)
 CMD_LIBLIB="${PATH_ME}/book2json.py"
 CMD_ALERT="${PATH_ME}/json2alert.py"
-CMD_VOICE2GH='/home/mah/script/voice2googlehome/voice2gh.sh'
+CMD_VOICE2GH="${PATH_ME}/../voice2googlehome/voice2gh.sh"
 
 TMP_FILE=$(mktemp)
 # 鉄板のtmpfile処理 https://fumiyas.github.io/2013/12/06/tempfile.sh-advent-calendar.html
 function func_atexit() {
-	#[[ -n ${DIR_TMP-} ]] && rm -rf "${DIR_TMP}"
 	[[ -n ${TMP_FILE-} ]] && rm -f "${TMP_FILE}"
 }
 trap func_atexit EXIT
@@ -29,3 +27,7 @@ function func_main () {
 export LIBLIB_USERNAME=''
 export LIBLIB_PASSWORD=''
 func_main
+
+# clear
+export -n LIBLIB_USERNAME
+export -n LIBLIB_PASSWORD
