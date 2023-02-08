06d0b41c2216807dda0dc6a8456c88654a940019
mah-jp <masahiko.ohkubo+github@gmail.com>
2023-02-08 23:15:34 +0900

    「利用可能」資料のHTML解析対応

--- book2json_opac.py
+++ book2json_opac.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
-# book2json_opac.py for 神戸市立図書館 (Ver.20230205)
+# book2json_opac.py for 神戸市立図書館 (Ver.20230208)
 # Usage: export LIBLIB_USERNAME=foo LIBLIB_PASSWORD=bar $0
 
+from bs4 import BeautifulSoup # pip3 install bs4
+from playwright.sync_api import Playwright, sync_playwright, expect # pip3 install playwright
 import datetime
 import json
 import os
 import re
 import sys
-from bs4 import BeautifulSoup # pip3 install bs4
-from playwright.sync_api import Playwright, sync_playwright, expect # pip3 install playwright
+import logging
+logger = logging.getLogger(__name__)
+
+URL_START: str = 'https://www.lib.city.kobe.jp/winj/sp/top.do?lang=ja' # winj スマートフォン版
 
 def run(playwright: Playwright, url: str, username: str, password: str) -> list:
 	browser = playwright.chromium.launch(headless=True)
@@ -32,7 +36,8 @@ def run(playwright: Playwright, url: str, username: str, password: str) -> list:
 	expect(page).to_have_title(re.compile('貸出状況一覧'))
 	soup = BeautifulSoup(page.content(), 'html.parser')
 	soup_checktext: str = soup.find('div', attrs={ 'class': 'strMain' }).text
-	if not re.search('該当するリストが存在しません。', soup_checktext):
+	logger.debug('borrowing soup_checktext={}'.format(cut_space(soup_checktext)))
+	if not re.search('該当するリストが存在しません。', soup_checktext): # 内容が表示されている
 		page.get_by_role('combobox', name='一覧表示件数').select_option('50')
 		expect(page).to_have_title(re.compile('貸出状況一覧'))
 		soup = BeautifulSoup(page.content(), 'html.parser')
@@ -48,7 +53,8 @@ def run(playwright: Playwright, url: str, username: str, password: str) -> list:
 	expect(page).to_have_title(re.compile('予約状況一覧'))
 	soup = BeautifulSoup(page.content(), 'html.parser')
 	soup_checktext: str = soup.find('div', attrs={ 'class': 'strMain' }).text
-	if not re.search('該当するリストが存在しません。', soup_checktext):
+	logger.debug('reservation soup_checktext={}'.format(cut_space(soup_checktext)))
+	if not re.search('該当するリストが存在しません。', soup_checktext): # 内容が表示されていたら
 		page.get_by_role('combobox', name='一覧表示件数').select_option('50')
 		expect(page).to_have_title(re.compile('予約状況一覧'))
 		soup = BeautifulSoup(page.content(), 'html.parser')
@@ -65,32 +71,40 @@ def parse_html(soup_books, mode: str) -> list:
 	books: list = []
 	i: int = 1
 	for book in soup_books:
-		title_raw: str = book.find('p', attrs={ 'class': 'title' }).find('span', attrs={ 'class': 'title' }).text
-		title: str = pickup_title(title_raw)
-		if mode == 'borrowing':
-			# 借りている資料
-			date_start_raw: str = book.find_all('p', attrs={ 'class': 'txt' })[1].text
-			date_start: str = pickup_date(date_start_raw, 'borrowing')
-			date_end_raw: str = book.find_all('p', attrs={ 'class': 'txt' })[2].text
-			date_end: str = pickup_date(date_end_raw, 'borrowing')
-			d: dict = { 'id': i, '書名': title, 'name': title, '貸出日': date_start, '返却予定日': date_end }
-			if date_end:
-				dt = datetime.datetime.strptime(date_end + ' 00:00:00 JST', '%Y-%m-%d %H:%M:%S %Z')
-				d['date_return'] = dt.isoformat() + '.000000+09:00'
-			books.append(d)
-		elif mode == 'reservation':
-			# 予約した資料
-			date_order_raw: str = book.find_all('p', attrs={ 'class': 'txt' })[1].text
-			date_order: str = pickup_date(date_order_raw, 'reservation')
-			library_raw: str = book.find_all('p', attrs={ 'class': 'txt' })[1].text
-			library: str = pickup_library(library_raw)
-			orderstatus_raw: str = book.find_all('p', attrs={ 'class': 'txt' })[2].text
-			orderstatus: str = pickup_orderstatus(orderstatus_raw)
-			d: dict = { 'id': i, '書名': title, 'name': title, '予約日': date_order, '受取館': library, '予約状態': orderstatus, 'ready': False }
-			if re.match('利用可能', orderstatus): # まだ動作未確認 Ref: https://www.city.kobe.lg.jp/documents/1570/k-lib_help.pdf#page=29
-				d['ready'] = True
-			books.append(d)
-		i += 1
+		logger.debug('{} book={}'.format(mode, cut_space(book.text)))
+		if book.find('p', attrs={ 'class': 'title' }): # 資料のタイトルがあったら
+			title_raw: str = book.find('p', attrs={ 'class': 'title' }).find('span', attrs={ 'class': 'title' }).text
+			logger.debug('{} title_raw={}'.format(mode, cut_space(title_raw)))
+			title: str = pickup_title(title_raw)
+			if mode == 'borrowing':
+				# 借りている資料
+				date_start_raw: str = book.find_all('p', attrs={ 'class': 'txt' })[1].text
+				logger.debug('{} date_start_raw={}'.format(mode, cut_space(date_start_raw)))
+				date_start: str = pickup_date(date_start_raw, 'borrowing')
+				date_end_raw: str = book.find_all('p', attrs={ 'class': 'txt' })[2].text
+				logger.debug('{} date_end_raw={}'.format(mode, cut_space(date_end_raw)))
+				date_end: str = pickup_date(date_end_raw, 'borrowing')
+				d: dict = { 'id': i, '書名': title, 'name': title, '貸出日': date_start, '返却予定日': date_end }
+				if date_end:
+					dt = datetime.datetime.strptime(date_end + ' 00:00:00 JST', '%Y-%m-%d %H:%M:%S %Z')
+					d['date_return'] = dt.isoformat() + '.000000+09:00'
+				books.append(d)
+			elif mode == 'reservation':
+				# 予約した資料
+				date_order_raw: str = book.find_all('p', attrs={ 'class': 'txt' })[1].text
+				logger.debug('{} date_order_raw={}'.format(mode, cut_space(date_order_raw)))
+				date_order: str = pickup_date(date_order_raw, 'reservation')
+				library_raw: str = book.find_all('p', attrs={ 'class': 'txt' })[1].text
+				logger.debug('{} library_raw={}'.format(mode, cut_space(library_raw)))
+				library: str = pickup_library(library_raw)
+				orderstatus_raw: str = book.find_all('p', attrs={ 'class': 'txt' })[2].text
+				logger.debug('{} orderstatus_raw={}'.format(mode, cut_space(orderstatus_raw)))
+				orderstatus: str = pickup_orderstatus(orderstatus_raw)
+				d: dict = { 'id': i, '書名': title, 'name': title, '予約日': date_order, '受取館': library, '予約状態': orderstatus, 'ready': False }
+				if re.match('利用可能', orderstatus): # Ref: https://www.city.kobe.lg.jp/documents/1570/k-lib_help.pdf#page=29
+					d['ready'] = True
+				books.append(d)
+			i += 1
 	return books
 
 def get_datetime() -> str:
@@ -124,25 +138,38 @@ def pickup_date(text: str, mode: str) -> str:
 	return r
 
 if __name__ == '__main__':
+	DIR_ME: str = os.path.dirname(os.path.abspath(__file__))
+	FILE_ME: str = os.path.splitext(os.path.basename(__file__))[0]
+	FILE_ME: str = os.path.basename(__file__)
+	FILE_LOGGING: str = os.path.join(DIR_ME, FILE_ME + '.log')
+	# loggingモジュールの設定 (debug, info, warning, error, critical)
+	logging.basicConfig(
+		# level=logging.DEBUG,
+		level=logging.ERROR,
+		format='[%(asctime)s] %(name)s[%(thread)d]:%(funcName)s:%(lineno)d <%(levelname)s> %(message)s',
+		# filename=FILE_LOGGING
+	)
+	logger.debug('FILE_LOGGING={}'.format(FILE_LOGGING))
+
 	try:
 		args = sys.argv
 		if not(os.environ.get('LIBLIB_USERNAME') and os.environ.get('LIBLIB_PASSWORD')):
-			print(('Usage: export LIBLIB_USERNAME=foo LIBLIB_PASSWORD=bar; %s ID ...' % args[0]), file=sys.stderr)
+			logging.ERROR('Usage: export LIBLIB_USERNAME=foo LIBLIB_PASSWORD=bar; {:s} ID ...'.format(args[0]))
 			sys.exit(1)
 		else:
 			username = os.environ.get('LIBLIB_USERNAME')
 			password = os.environ.get('LIBLIB_PASSWORD')
 			if len(username) * len(password) == 0:
-				print(('ERROR: LIBLIB_USERNAME and/or LIBLIB_PASSWORD is empty.' % args[0]), file=sys.stderr)
+				logging.ERROR('ERROR: LIBLIB_USERNAME and/or LIBLIB_PASSWORD is empty.'.format())
 				sys.exit(2)
-
-		url: str = 'https://www.lib.city.kobe.jp/winj/sp/top.do?lang=ja'
+		url: str = URL_START
+		logger.debug('url={}'.format(url))
 		d: dict = { 'status': False, 'url': url }
 		with sync_playwright() as playwright:
 			r1, r2 = run(playwright, url, username, password)
+			logger.debug('borrowing={}, reservation={}'.format(r1, r2))
 			d.update({ 'status': True, 'borrowing': r1, 'reservation': r2 })
 		print(json.dumps(d))
-
 	except KeyboardInterrupt as e:
-		print(e)
+		logging.ERROR('{}'.format(e))
 		raise
